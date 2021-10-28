2f6d80a685e5a4d06a5f661c8f4ea29987092639
naveenreddy039 <92319818+naveenreddy039@users.noreply.github.com>
2021-10-28 16:04:05 +0530

    Division.cs

---
+++ Subs/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multiplication
+{
+    public class Division
+    {
+        int totalValue;
+
+        public int isDiv(int num1, int num2)
+        {
+            return totalValue = num1 / num2;
+        }
+    }
+}
