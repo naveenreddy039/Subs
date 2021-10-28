7468868ee40bcd55feaaf884c738eb410e3f16b3
naveen <naveenreddy.k07@gmail.com>
2021-10-28 14:59:39 +0530

    firat commit

---
+++ Subs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Subs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Substraction objSubstraction = new Substraction();
+            Console.WriteLine("total values" + objSubstraction.isDiff(30, 20));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Subs/Substraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subs
+{
+    public class Substraction
+    {
+        int totalValues;
+
+        public int isDiff(int num1,int num2)
+        {
+            return totalValue = num1 - num2;
+        }
+    }
+}
