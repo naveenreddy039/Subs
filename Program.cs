6f6ff914ff49d90589c9928e75fd52a0f62b97b9
naveenreddy039 <92319818+naveenreddy039@users.noreply.github.com>
2021-10-28 19:55:09 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division objDivision = new Division();
+            Console.WriteLine("total value" + objDivision.isDiv(30, 20));
+            Console.ReadKey();
+        }
+    }
+}
