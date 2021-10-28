ff13053c2952c7ca095faef44fe504d57495f2fe
naveenreddy039 <92319818+naveenreddy039@users.noreply.github.com>
2021-10-28 15:58:28 +0530

    Multiplication.cs

---
+++ Multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multiplication
+{
+    public class Multiplication
+    {
+        int totalValue;
+
+        public int isMul(int num1,int num2)
+        {
+            return totalValue = num1 * num2;
+        }
+    }
+}
